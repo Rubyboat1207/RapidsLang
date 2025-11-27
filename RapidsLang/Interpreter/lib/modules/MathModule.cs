ca72ce60f9a39246db5e21bbf7439aa11f1fba76
Rudy Soliz <rudy@rudysoliz.com>
2025-11-26 21:30:22 -0600

    add mapping function

--- RapidsLang/Interpreter/lib/modules/MathModule.cs
+++ RapidsLang/Interpreter/lib/modules/MathModule.cs
@@ -24,8 +24,40 @@ public class MathModule : Module
         RapidsPrimitiveType.Number
     );
 
+    private static void Map(RapidsInterpreter interpreter)
+    {
+        using var utils = interpreter.GetNativeUtil().GuaranteeReturn();
+
+        var outEnd = utils.LatestNumber();
+        var outStart = utils.LatestNumber();
+
+        var inEnd = utils.LatestNumber();
+        var inStart = utils.LatestNumber();
+
+        var input = utils.LatestNumber();
+
+        if (outEnd == null || outStart == null || inEnd == null || inStart == null || input == null)
+        {
+            return;
+        }
+        
+        utils.Return(outStart.Value + ((outEnd.Value - outStart.Value) / (inEnd.Value - inStart.Value)) * (input.Value - inStart.Value));
+    }
+    
+    private static readonly RapidsType MapType = new RapidsFunctionType(
+        [
+            new("input", RapidsPrimitiveType.Number),
+            new("inStart", RapidsPrimitiveType.Number),
+            new("inEnd", RapidsPrimitiveType.Number),
+            new("outStart", RapidsPrimitiveType.Number),
+            new("outEnd", RapidsPrimitiveType.Number),
+        ],
+        RapidsPrimitiveType.Number
+    );
+
     public override ModuleExports Exports { get; } = new(new()
     {
         {"round", new(RapidsFunctionReferenceVariable.OfNative(Round, RoundType), RoundType)},
+        {"map", new(RapidsFunctionReferenceVariable.OfNative(Map, MapType), MapType)}
     });
 }
